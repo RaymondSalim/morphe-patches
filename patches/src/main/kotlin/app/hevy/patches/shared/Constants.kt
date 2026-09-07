f9b802564cf30311bd752cfe2024428dfe5044f7
Raymond Salim <rsalim@domaintools.com>
2026-09-08 03:00:40 +0700

    chore: Update Hevy compatibility targets

--- patches/src/main/kotlin/app/hevy/patches/shared/Constants.kt
+++ patches/src/main/kotlin/app/hevy/patches/shared/Constants.kt
@@ -5,16 +5,23 @@ import app.morphe.patcher.patch.AppTarget
 import app.morphe.patcher.patch.Compatibility
 
 object Constants {
-    val COMPATIBILITY_HEVY = Compatibility(
+    val COMPATIBILITY_HEVY_APKM = Compatibility(
+        name = "Hevy - Gym Log Workout Tracker",
+        packageName = "com.hevy",
+        apkFileType = ApkFileType.APKM,
+        appIconColor = 0x1C1C1E, // Dark background of the Hevy launcher icon.
+        targets = listOf(
+            AppTarget(version = "3.0.11")
+        )
+    )
+
+    val COMPATIBILITY_HEVY_APK = Compatibility(
         name = "Hevy - Gym Log Workout Tracker",
         packageName = "com.hevy",
         apkFileType = ApkFileType.APK,
-        appIconColor = 0xFF6C63, // Hevy app icon color (orange/red)
+        appIconColor = 0x1C1C1E,
         targets = listOf(
-            AppTarget(
-                version = "3.0.11"
-            )
-            // Add more versions as tested
+            AppTarget(version = "3.0.11")
         )
     )
 }
